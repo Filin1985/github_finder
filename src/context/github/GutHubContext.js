63cb1d5e2779de5c3822858eebe38889bc4daac2
Marat Ikhsanov <ihsmen@gmail.com>
2023-03-23 21:37:45 +0300

    feat: Add GET_REPOS reducer

--- src/context/github/GutHubContext.js
+++ src/context/github/GutHubContext.js
@@ -10,6 +10,7 @@ export const GitHubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   }
 
@@ -55,6 +56,31 @@ export const GitHubProvider = ({ children }) => {
     }
   }
 
+  const getUserRepos = async (login) => {
+    setLoading(true)
+
+    const params = new URLSearchParams({
+      sort: 'created',
+      per_page: 10,
+    })
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    )
+
+    const data = await response.json()
+
+    dispatch({
+      type: 'GET_REPOS',
+      payload: data,
+    })
+  }
+
   const clearUsers = () => {
     dispatch({
       type: 'CLEAR_USERS',
@@ -73,9 +99,11 @@ export const GitHubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         getUser,
         clearUsers,
+        getUserRepos,
       }}
     >
       {children}
